dd0b5846412705c646c5f7d1f028120362abe7e7
USER <USER@LAPTOP-DJQE6P35>
2020-10-15 13:26:52 +0530

    add branch

---
+++ EmployeeManagement/src/com/lti/model/Department.java
@@ -0,0 +1,14 @@
+package com.lti.model;
+
+public class Department {
+	String deptname;
+
+	public String getDeptname() {
+		return deptname;
+	}
+
+	public void setDeptname(String deptname) {
+		this.deptname = deptname;
+	}
+
+}
